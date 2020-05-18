179fe245f92d73bb65290089dd683faf264f0deb
Adam Thalhammer <adam.thalhammer@redbubble.com>
2020-05-18 22:21:49 +1000

    Wait for stack create/delete to complete
    Also:
    
    * fixed some shellcheck warnings
    * handle deleting multiple old stacks
    * create a variable for key_file
    * remove unecessary exports
    * jq instead of awk
    * set -e: exit on error
    * set -u: error on undefined variable
    * set -o pipefail: error if a command in a pipe fails

--- new_cluster.sh
+++ new_cluster.sh
@@ -4,39 +4,33 @@
 # - The following are pre-installed: python-openstackclient, python-heatclient, awk
 # - The host is connected to the Unimelb VPN
 
-# Check for existing cluster
-cluster_name=$(openstack stack list | awk -F'|' ' NR > 3 && !/^+--/ { print $3} ')
-cluster_name_trimmed="$(echo -e "${cluster_name}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+set -euo pipefail
 
-# Remove existing cluster
-if [ -z "$cluster_name" ]
+stack_name=$1
+key_file="$HOME/.ssh/id_group71"
+
+# Check for existing clusters
+old_stack_names=$(openstack stack list -f json | jq --raw-output '.[]."Stack Name"')
+
+# Remove existing clusters
+if [[ -z "$old_stack_names" ]]
 then
       echo "No current running cluster"
 else
-      echo "Found current running cluster: $cluster_name_trimmed"
-      echo "Deleting cluster"
-      openstack stack delete $cluster_name_trimmed
+      echo "Deleting current running clusters: $old_stack_names"
+      echo "$old_stack_names" | xargs openstack stack delete --wait
 fi
 
 # Create new cluster
-export STACK_NAME=$1
-echo 'Creating the following stack:'
-echo ${STACK_NAME}
-openstack stack create --template heat/cluster.yaml --environment heat/cluster.params.yaml ${STACK_NAME}
-
-# Wait two minutes for stack creation to complete
-echo 'Waiting for stack to be created'
-sleep 120
+echo "Creating the following stack: $stack_name"
+openstack stack create --wait --template heat/cluster.yaml --environment heat/cluster.params.yaml "$stack_name"
 
 # Copy the private key to local ssh keystore
-openstack stack output show -f json ${STACK_NAME} private_key | jq -r '.output_value' > ${HOME}/.ssh/id_group71
-chmod 600 ~/.ssh/id_group71
+openstack stack output show -f json "$stack_name" private_key | jq --raw-output '.output_value' > "$key_file"
+chmod 600 "$key_file"
 
 # Update the ansible hosts file
-python3 ansible/inventory/update_hosts.py
+ansible/inventory/update_hosts.py
 
 # Run the ansible playbook
-export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook ansible/site.yml -i ansible/inventory/hosts.ini --key-file ~/.ssh/id_group71
-
-echo 'DONE'
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ansible/site.yml -i ansible/inventory/hosts.ini --key-file "$key_file"
