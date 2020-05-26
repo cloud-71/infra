b9e410fab39fcd578bc21f03ea1eae795efd7084
Adam Thalhammer <adam.thalhammer@redbubble.com>
2020-05-26 16:26:06 +1000

    Cross-platform way of getting absolute path

--- kubernetes/kustomize_build.sh
+++ kubernetes/kustomize_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-kubernetes_dir=$(dirname "$(readlink -f "$0")")
+kubernetes_dir="$(cd "$(dirname "$0")" && pwd)"
 export KUSTOMIZE_PLUGIN_HOME="$kubernetes_dir/kustomize_plugins"
 exec kustomize build --enable_alpha_plugins "$@"
